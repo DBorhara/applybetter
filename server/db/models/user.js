59aaed6c3b09681a3fcf9aea638ce2c2d4544368
Depak Borhara <depakborhara@gmail.com>
2020-10-30 02:10:04 -0400

    First commit with boilerplate models working

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -16,6 +16,21 @@ const User = db.define('user', {
       return () => this.getDataValue('password')
     }
   },
+  firstName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  appLinks: {
+    type: Sequelize.ARRAY(Sequelize.TEXT)
+  },
   salt: {
     type: Sequelize.STRING,
     // Making `.salt` act like a function hides it when serializing to JSON.
